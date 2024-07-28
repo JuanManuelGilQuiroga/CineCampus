8d9fc6077a0065a9eb6a5185e8d5b57bad98f2d6
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-07-28 16:39:51 -0500

    docs: :memo: Comments about gestionFuncion completed

--- js/module/casosDeUso/gestionFuncion/funcionRun.js
+++ js/module/casosDeUso/gestionFuncion/funcionRun.js
@@ -1,6 +1,15 @@
 import { ObjectId } from "mongodb";
 import { insertFuncion } from "./funcionController.js";
 
+/**
+ * Parámetros de la función a insertar.
+ * @type {Object}
+ * @property {ObjectId} pelicula_id - El ID de la película asociada a la función.
+ * @property {ObjectId} sala_id - El ID de la sala donde se proyectará la película.
+ * @property {Date} fecha_hora_inicio - La fecha y hora de inicio de la función.
+ * @property {Date} fecha_hora_final - La fecha y hora de finalización de la función.
+ * @property {number} precio_COP - El precio en COP de la función.
+ */
 let funcionInsertar = {
     pelicula_id: new ObjectId('66a597b03d45ef35a8b018ac'),
     sala_id: new ObjectId('66a58fab8a7ddb4b6799c27a'),
@@ -9,4 +18,5 @@ let funcionInsertar = {
     precio_COP: 7000
 }
 
+// Llama a la función para insertar la función y muestra el resultado
 console.log(await insertFuncion(funcionInsertar))
--- js/module/clasesColecciones/sala.js
+++ js/module/clasesColecciones/sala.js
@@ -21,11 +21,20 @@ export class Sala extends Connect {
         return this;
     }
 
+    /**
+     * Obtiene todas las salas de la colección.
+     * @param {Object} salaParametro - El objeto que especifica el filtro para buscar las salas
+     * @returns {Promise<Array>} Una promesa que resuelve con un array de documentos de salas.
+     */
     async findSala(salaParametro) {
         let res = await this.collection.find(salaParametro).toArray()
         return res
     }
 
+    /**
+     * @param {Object} salaParametro - El objeto que especifica el filtro para buscar la sala
+     * @returns {Promise<Object>} Una promesa que resuelve con el documento de la sala buscada
+     */
     async findSalaById(salaParametro) {
         let res = await this.collection.findOne(salaParametro)
         return res
