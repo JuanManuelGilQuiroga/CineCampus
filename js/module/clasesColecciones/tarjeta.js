ba23543dc811f227135ed508b5b8624b31bc0a62
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-07-29 16:41:08 -0500

    feat: :sparkles: Creation of the fields for the 'tarjeta' collection

---
+++ js/module/clasesColecciones/tarjeta.js
@@ -0,0 +1,23 @@
+import { Connect } from '../../../helpers/db/connect.js';
+
+/**
+ * Clase `Sala` para gestionar operaciones relacionadas con la colección de Salas en la base de datos.
+ * Hereda de la clase `Connect`, que maneja la conexión a la base de datos.
+ */
+export class Tarjeta extends Connect {
+    static instanceTarjeta; // Instancia Singleton de la clase Tarjeta
+    
+    /**
+     * Crea una instancia de la clase `Tarjeta`.
+     * Implementa el patrón Singleton para garantizar que solo haya una instancia de esta clase.
+     */
+    constructor() {
+        if (typeof Tarjeta.instanceTarjeta === "object") {
+            return Tarjeta.instanceTarjeta;
+        }
+        super();
+        this.collection = this.db.collection("tarjeta");
+        Tarjeta.instanceSala = this;
+        return this;
+    }
+}
