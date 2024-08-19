f7af73fbd69aeb6b0d03e99e2e371742f77d8db4
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-08-19 17:03:42 -0500

    feat: :sparkles: Creation of the crearUsuario almost completed because the card logic is missing

--- server/dto/usuario.dto.js
+++ server/dto/usuario.dto.js
@@ -1,8 +1,44 @@
 module.exports = class UsuarioDTO {
+    typeToRole(arg){
+        if(arg.tipo == "Admin"){
+            arg.tipo = "Administrador"
+        } else if(arg.tipo == "Estandar"){
+            arg.tipo = "usuarioEstandar"
+        } else if(arg.tipo == "VIP"){
+            arg.tipo = "usuarioVIP"
+        }
+        return {
+            ...arg,
+            tipo: arg.tipo
+        }
+    }
+
     templateNotUsers(){
         return {
             status: 404,
             message: "No hay usuarios registrados"
+        }
+    }
+
+    templateUserError(arg){
+        return {
+            status: 500,
+            message: "Ocurrio un error",
+            data: arg
+        }
+    }
+
+    templateExistUser(arg){
+        return {
+            status: 200,
+            data: arg
+        }
+    }
+
+    templateUserSaved(arg){
+        return {
+            status: 201,
+            data: arg
         }
     }
 
--- server/router.js
+++ server/router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 //const path = require('path');
-const { listarTodosLosUsuarios, listarUsuariosPorTipo } = require('./controller/usuario.controller');
-const {  usuarioValidationRulesCreation, usuarioValidationRulesFindType, usuarioValidationEmpty } = require('./validators/usuario.validator');
+const { listarTodosLosUsuarios, listarUsuariosPorTipo, crearUsuario } = require('./controller/usuario.controller');
+const { usuarioValidationRulesCreation, usuarioValidationRulesFindType, usuarioValidationEmpty } = require('./validators/usuario.validator');
 
-
-router.get("/users/v1", usuarioValidationRulesFindType(), listarUsuariosPorTipo);
-router.get("/users/v2", usuarioValidationEmpty(), listarTodosLosUsuarios);
+router.get("/users/v1", usuarioValidationRulesCreation(), crearUsuario);
+router.get("/users/v2", usuarioValidationRulesFindType(), listarUsuariosPorTipo);
+router.get("/users/v3", usuarioValidationEmpty(), listarTodosLosUsuarios);
 
 
 module.exports = router;
