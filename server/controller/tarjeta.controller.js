9acd087d14e156e7542001ec171f7e9a1e128c2e
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-08-21 21:51:39 -0500

    feat: :sparkles: Creation of the verificarAsientos completed

--- server/controller/tarjeta.controller.js
+++ server/controller/tarjeta.controller.js
@@ -204,36 +204,36 @@ const crearTarjeta = async(req, res) => {
     let data = tarjetaDTO.usuarioIdToIdKey({...reqObjectId});
     let resModel = await objCliente.findOneClienteById(data);
     data = (resModel) ? usuarioDTO.templateExistUser(resModel) : usuarioDTO.templateNotUsers();
-    if(data.status == 404) res.status(data.status).json(data);
+    if(data.status == 404) return res.status(data.status).json(data);
     let usuario = resModel
     if(data.status == 200) resModel = await objTarjeta.findOneTarjetaByNumber(req.body);
     data = (resModel) ? tarjetaDTO.templareTarjetaDuplicated(resModel) : tarjetaDTO.templateNotCards();
-    if(data.status == 409) res.status(data.status).json(data);
+    if(data.status == 409) return res.status(data.status).json(data);
     let mongoUser = usuarioDTO.mongoUserToObject(process.env.MONGO_USER);
     resModel = await objCliente.findOneClienteByNickOrEmail(mongoUser);
     if(resModel.tipo != "Admin"){
         data = (process.env.MONGO_USER != usuario.nick) ? tarjetaDTO.templateBadCredentials() : tarjetaDTO.templateContinue();
-        if(data.status == 401) res.status(data.status).json(data);
+        if(data.status == 401) return res.status(data.status).json(data);
     }
     data = (usuario.tipo == "Estandar") ? tarjetaDTO.templateContinue() : usuarioDTO.templateBadRequest();
-    if(data.status == 400) res.status(data.status).json(data);
+    if(data.status == 400) return res.status(data.status).json(data);
     usuario = usuarioDTO.typeToRole(usuario);
     let newUsuario = usuarioDTO.changeRole({...usuario});
     console.log("usuario", usuario)
     console.log("newUsuario", newUsuario)
     resModel = await objCliente.revokeRolesFromUsuario(usuario);
     data = (resModel.ok) ? usuarioDTO.templateExistUser(resModel) : usuarioDTO.templateUserError(resModel);
-    if(data.status == 500) res.status(data.status).json(data);
+    if(data.status == 500) return res.status(data.status).json(data);
     if(data.status == 200) resModel = await objCliente.grantRolesToUsuario(newUsuario)
     data = (resModel.ok) ? usuarioDTO.templateExistUser(resModel) : usuarioDTO.templateUserError(resModel)
-    if(data.status == 500) res.status(data.status).json(data);
+    if(data.status == 500) return res.status(data.status).json(data);
     if(data.status == 200) newUsuario = usuarioDTO.roleToType(newUsuario);
     resModel = await objCliente.updateCliente(newUsuario);
     data = (resModel.modifiedCount) ? usuarioDTO.templateExistUser(resModel) : usuarioDTO.templateUserError(resModel);
-    if(data.status == 500) res.status(data.status).json(data);
+    if(data.status == 500) return res.status(data.status).json(data);
     if(data.status == 200) resModel = await objTarjeta.insertTarjeta(reqObjectId);
     data = (resModel.acknowledged) ? tarjetaDTO.templateTarjetaSaved(reqObjectId) : tarjetaDTO.templateTarjetaError(resModel);
-    if(data.status == 500) res.status(data.status).json(data);
+    if(data.status == 500) return res.status(data.status).json(data);
     res.status(data.status).json(data);
 }
 
--- server/controller/usuario.controller.js
+++ server/controller/usuario.controller.js
@@ -162,15 +162,15 @@ const crearUsuario = async (req, res) => {
     const obj = new Usuario();
     let resModel = await obj.findOneClienteByNickOrEmail(req.body);
     let data = (resModel) ? usuarioDTO.templateExistUser(resModel) : usuarioDTO.templateNotUsers();
-    if(data.status == 200) res.status(data.status).json(data);
+    if(data.status == 200) return res.status(data.status).json(data);
     if(data.status == 404) resModel = await obj.saveUsuario(req.body);
     data = (resModel.acknowledged) ? usuarioDTO.templateUserSaved(req.body) : usuarioDTO.templateUserError(resModel);
-    if(data.status == 500) res.status(data.status).json(data);
+    if(data.status == 500) return res.status(data.status).json(data);
     if(data.status == 201) data = usuarioDTO.typeToRole(req.body);
     if(data.tipo == "Administrador"){resModel = await obj.createUsuarioAdmin(data)} else{resModel = await obj.createUsuarioCliente(data)};
     data = (resModel.ok) ? usuarioDTO.templateUserSaved(req.body) : usuarioDTO.templateUserError(resModel);
-    if(data.status == 500) res.status(data.status).json(data);
-    res.status(data.status).json(data);
+    if(data.status == 500) return res.status(data.status).json(data);
+    return res.status(data.status).json(data);
 }
 
 const listarUsuariosPorTipo = async(req, res) => {
@@ -180,15 +180,17 @@ const listarUsuariosPorTipo = async(req, res) => {
     const obj = new Usuario();
     let resModel = await obj.findClientesByType(req.query.tipo);
     let data = (resModel.length) ? usuarioDTO.templateListUsers(resModel) : usuarioDTO.templateNotUsers(resModel);
-    res.status(data.status).json(data);
+    return res.status(data.status).json(data);
 }
 
 const listarTodosLosUsuarios = async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) return res.status(400).json({errors: errors.array() });
     const usuarioDTO = new UsuarioDTO();
     const obj = new Usuario();
     let resModel = await obj.findClientes();
     let data = (resModel.length) ? usuarioDTO.templateListUsers(resModel) : usuarioDTO.templateNotUsers(resModel);
-    res.status(data.status).json(data);
+    return res.status(data.status).json(data);
 }
 
 
