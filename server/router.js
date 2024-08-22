1954b9120442cc17b080a527b11e08d87dca2021
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-08-21 23:05:24 -0500

    feat: :sparkles: Creation of the detallesPelicula completed

--- server/router.js
+++ server/router.js
@@ -5,14 +5,15 @@ const { crearTarjeta } = require('./controller/tarjeta.controller');
 const { tarjetaValidationRulesCreation } = require('./validators/tarjeta.validator');
 const { verificarAsientos } = require('./controller/funcion.controller')
 const { funcionValidationRulesAsientos } = require('./validators/funcion.validator');
-const { listarPeliculas } = require('./controller/pelicula.controller');
-const { peliculaValidationEmpty } = require('./validators/pelicula.validator');
+const { listarPeliculas, detallesPelicula } = require('./controller/pelicula.controller');
+const { peliculaValidationEmpty, peliculaValidationRulesDetalles } = require('./validators/pelicula.validator');
 
 router.post("/users/v1", usuarioValidationRulesCreation(), crearUsuario);
 router.get("/users/v2", usuarioValidationRulesFindType(), listarUsuariosPorTipo);
 router.get("/users/v3", usuarioValidationEmpty(), listarTodosLosUsuarios);
 router.post("/cards/v1", tarjetaValidationRulesCreation(), crearTarjeta);
 router.get("/functions/v1", funcionValidationRulesAsientos(), verificarAsientos);
-router.get("/movies/v1", peliculaValidationEmpty(), listarPeliculas)
+router.get("/movies/v1", peliculaValidationEmpty(), listarPeliculas);
+router.get("/movies/v2", peliculaValidationRulesDetalles(), detallesPelicula)
 
 module.exports = router;
