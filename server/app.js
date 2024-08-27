e0bb5b1114d4a1a1b1d9b9543d3ce31ddb8e6e60
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-08-27 12:05:52 -0500

    feat: :heavy_plus_sign: Install of cors and implementation of that in the app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 const router = require('./router');
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 app.use('/css', express.static(path.join(__dirname, process.env.EXPRESS_STATIC, 'css')));
