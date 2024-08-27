82e16f375a02b8a087b07e137d7f24507dd0aa53
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-08-26 22:48:38 -0500

    feat: :sparkles: Creation of the header in the index completed
    Also i created an env file in the public folder but this desition isnt clear

--- public/js/main.jsx
+++ public/js/main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HeaderUser } from "./components/headerUser";
 import { StrictMode } from "react";
+import { HeaderIndex } from "./components/headerIndex";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-        <HeaderUser />
+        <div className="w-[80vw] h-[15vh] flex flex-col justify-between">
+            <HeaderIndex />
+            <div className="border-[1px] border-white rounded-[10px] flex items-center p-4">
+                <i class='bx bx-search' style={{color: "white", fontSize: "20px"}}></i>
+                <input type="text" placeholder="Search movie, cinema, gender..." className="px-2 border-none focus:outline-none"/>
+            </div>
+        </div>
     </StrictMode>
 )
