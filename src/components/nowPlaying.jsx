be2636895e6d0d258366a952560b9daed3aa7f0c
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-08-28 01:30:31 -0500

    feat: :sparkles: Carousel component completed

--- src/components/nowPlaying.jsx
+++ src/components/nowPlaying.jsx
@@ -1,36 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { Carousel } from "./carousel";
 import "../../css/style.css"
 
-export const consulta = async () => {
-    let listarPeliculas = await fetch(`http://localhost:${import.meta.env.VITE_PORT_BACKEND}/movies/v3`).then(res=> res.json());
-    let data = await listarPeliculas;
-    console.log(data)
-    return data
-}
 
 export function NowPlaying () {
-    
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-            consulta().then(res => setData(res.data))
-        }, [])
-    console.log("hola",data)
     return (
         <React.Fragment>
             <div className="flex justify-between w-[80vw] pt-8">
                 <h1 className="text-white text-lg">Now Playing</h1>
                 <strong className="text-red-600 text-lg">See all</strong>
             </div>
-            <div>
-                {
-                    data.map((movie) => {
-                        return <div key={movie._id}>
-                            <img src={movie.imagen} alt="" />
-                        </div>
-                    })
-                }
-            </div>
+            <Carousel />
         </React.Fragment>
     )
 }
