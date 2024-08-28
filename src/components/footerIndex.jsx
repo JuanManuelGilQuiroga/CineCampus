11273f7687456f4cc48a13e4d5db1e7149912045
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-08-28 03:02:22 -0500

    feat: :sparkles: Creation of the footer in the index view completed

---
+++ src/components/footerIndex.jsx
@@ -0,0 +1,24 @@
+import "../../css/style.css"
+
+export function FooterIndex () {
+    return (
+        <div className="flex fixed bottom-0 items-center justify-around w-full h-[12vh] bg-custom-black-232323 rounded-tl-3xl rounded-tr-3xl">
+            <div className="flex flex-col items-center bg-transparent">
+                <i className='bx bxs-home' style={{ color: 'red', fontSize: "20px", background: "transparent" }}></i>
+                <span className="text-red-600 bg-transparent">Home</span>
+            </div>
+            <div className="flex flex-col items-center bg-transparent">
+                <i class='bx bx-search' style={{ color: 'white', fontSize: "20px", background: "transparent" }}></i>
+                <span className="text-white bg-transparent">Search</span>
+            </div>
+            <div className="flex flex-col items-center bg-transparent">
+                <i class='bx bxs-coupon' style={{ color: 'white', fontSize: "20px", background: "transparent" }}></i>
+                <span className="text-white bg-transparent">Tickets</span>
+            </div>
+            <div className="flex flex-col items-center bg-transparent">
+                <i class='bx bxs-user-circle' style={{ color: 'white', fontSize: "20px", background: "transparent" }}></i>
+                <span className="text-white bg-transparent">Profile</span>
+            </div>
+        </div>
+    )
+}
