23112dc6053ad3938416a7e0fab413ae7d46a959
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-08-29 21:20:34 -0500

    refactor: :bug: Some arrangements for the change works of the change date in the functions

--- src/components/functionCart.jsx
+++ src/components/functionCart.jsx
@@ -1,10 +1,10 @@
 import "../../css/style.css";
 
-export function FunctionCard ({fecha, precio, tipo}) {
+export function FunctionCard ({fecha, precio, tipo, onFunctionClick, index}) {
     let horas = new Date(fecha).getHours()
     let minutos = new Date(fecha).getMinutes()
     return (
-        <button className="flex flex-col items-center justify-around w-[40%] rounded-xl bg-white py-2 px-4">
+        <button className="flex flex-col items-center justify-around w-[40%] rounded-xl bg-white py-2 px-4" onClick={() => onFunctionClick(index)}>
             <strong className="text-[1rem] bg-transparent">{`${horas}:${minutos}`}</strong>
             <span className="text-[0.5rem] bg-transparent">{`$${precio}-${tipo}`}</span>
         </button>
--- src/components/functionDayCard.jsx
+++ src/components/functionDayCard.jsx
@@ -1,6 +1,6 @@
 import "../../css/style.css";
 
-export function FunctionDayCard ({fecha}) {
+export function FunctionDayCard ({fecha, onDayClick, index}) {
     let diasSemana = {
         0: "Sun",
         1: "Mon",
@@ -11,7 +11,7 @@ export function FunctionDayCard ({fecha}) {
         6: "Sat"
     }
     return (
-        <button className="flex flex-col items-center justify-between rounded-xl bg-white p-4">
+        <button key={index} className="flex flex-col items-center justify-between rounded-xl bg-white p-4" onClick={() => {onDayClick(index)}}>
                 <span className="text-[1rem] bg-transparent">{diasSemana[new Date(fecha).getDay()]}</span>
                 <strong className="text-[1.5rem] bg-transparent">{new Date(fecha).getDate()}</strong>
         </button>
