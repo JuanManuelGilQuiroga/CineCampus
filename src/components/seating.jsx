d1cd6ab07fd0b4de6ebf1bf85077a33bf9fb6552
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-08-31 19:42:03 -0500

    refactor: :bug: The funciones and seating components had to be complemented with the useEffect hook

--- src/components/seating.jsx
+++ src/components/seating.jsx
@@ -7,7 +7,7 @@ export function Seating ({resFunc, row, day, func, onSeatClick, selectedSeats, s
             <div className="w-[100%] flex justify-center items-center gap-1.5">
                 {resFunc[day].funciones[func].sala_asientos.map(seat => {
                     if(seat.includes(row)){
-                        return <div key={seat} className={`h-8 w-8 flex justify-center items-center rounded-lg bg-custom-black-323232 seat ${selectedSeats.includes(seat) ? "bg-custom-red" : ""}`} onClick={() => {onSeatClick(seat), setPriceClick(selectedSeats, id=resFunc[day].funciones[func].funcion_id, seat)}}>
+                        return <div key={seat} className={`h-8 w-8 flex justify-center items-center rounded-lg bg-custom-black-323232 seat ${selectedSeats.includes(seat) ? "bg-custom-red" : ""}`} onClick={() => onSeatClick(seat)}>
                             <strong className={`text-[1rem] bg-transparent seat ${selectedSeats.includes(seat) ? "text-white" : "text-transparent"}`}>{seat.split("")[1]}</strong>
                         </div>
                     }
