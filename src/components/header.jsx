f1685c2dca200116820f23b4a7be69deb9d71284
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-09-02 11:57:57 -0500

    feat: :zap: Some arrangements for the ticket view

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import "../../css/style.css";
 export function Header ({titulo}) {
     return (
         <div className="bg-transparent flex items-center justify-around w-full h-[8vh]">
-                <Link to={-1} className='bg-transparent'>
+                <Link to={(titulo == "Ticket" ? "/" : "-1")} className='bg-transparent'>
                     <i className='bx bxs-chevron-left' style={{color: "#9CA3AF", background: "transparent"}}></i>
                 </Link>
                 <strong className="text-custom-gray-9CA3AF bg-transparent">{titulo}</strong>
--- src/views/ticket.jsx
+++ src/views/ticket.jsx
@@ -44,51 +44,57 @@ export const Ticket = () => {
     return (
         <>
             <Header titulo="Ticket"></Header>
-            <div className='flex flex-col justify-between items-center bg-white h-[70vh] w-[80vw] mt-10 rounded-2xl p-8'>
-                <div className='bg-transparent flex flex-col justify-between h-[25%] w-[100%]'>
-                    <img src="../../../public/campusCineLogo.jpg" alt="" className='bg-transparent object-cover h-[70%] border-black border-[3px] rounded-xl'/>
-                    <strong className='bg-transparent text-[1.2rem]'>Puss in Boots The Last Wish</strong>
-                    <span className='bg-transparent text-custom-black-323232 text-[0.8rem]'>Show this ticket at the entrance</span>
-                </div>
-                <hr className='bg-custom-gray-9CA3AF w-[100%] h-[1px]'/>
-                <div className='bg-transparent flex justify-between items-center w-[100%] h-[10%]'>
-                    <div className='bg-transparent'>
-                        <p className='bg-transparent font-medium text-custom-black-232323'>Cinema</p>
-                        <strong className='bg-transparent text-custom-black-232323'>CINECAMPUS</strong>
-                    </div>
-                    <img src="../../../public/campusCineLogo.jpg" alt="" className='bg-transparent object-cover h-[100%] rounded-lg border-black border-[3px]'/>
-                </div>
-                <div className='grid grid-cols-2 grid-cols-[1fr,0.8fr] grid-rows-3 bg-transparent w-[100%] gap-4'>
-                    <div className='bg-transparent'>
-                        <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Date</p>
-                        <p  className='bg-transparent font-bold text-[0.8rem]'>Sun, Feb 12th 2023</p>
-                    </div>
-                    <div className='bg-transparent'>
-                        <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Time</p>
-                        <p  className='bg-transparent font-bold text-[0.8rem]'>13:00</p>
-                    </div>
-                    <div className='bg-transparent'>
-                        <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]font-bold text-[0.8rem]'>CineCampus #</p>
-                        <p  className='bg-transparent font-bold text-[0.8rem]'>CineCampus</p>
-                    </div>
-                    <div className='bg-transparent'>
-                        <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Seat</p>
-                        <p  className='bg-transparent font-bold text-[0.8rem]'>C5</p>
-                    </div>
-                    <div className='bg-transparent'>
-                        <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Total Cost</p>
-                        <p  className='bg-transparent font-bold text-[0.8rem]'>$18.000</p>
-                    </div>
-                    <div className='bg-transparent'>
-                        <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Order ID</p>
-                        <p  className='bg-transparent font-bold text-[0.8rem]'>1234567890123456</p>
-                    </div>
-                </div>
-                <hr className='bg-custom-black-121212 flex w-[80vw] h-[1px] m-0 p-0'/>
-                <div className='w-[100%] h-[15%] bg-transparent flex justify-center items-center'>
-                    <img alt='Barcode Generator TEC-IT'
-                    src='https://barcode.tec-it.com/barcode.ashx?data=978020137962&code=EAN13&translate-esc=on' className='object-contain h-[100%]'/>
-                </div>
+            <div className='flex flex-col mb-10'>
+                {data.map(boleta => {
+                    return ( 
+                        <div className='flex flex-col justify-between items-center bg-white h-[70vh] w-[80vw] mt-10 rounded-2xl p-8'>
+                            <div className='bg-transparent flex flex-col justify-between h-[25%] w-[100%]'>
+                                <img src={boleta.pelicula_imagen} alt="" className='bg-transparent object-cover h-[70%] rounded-xl'/>
+                                <strong className='bg-transparent text-[1.2rem]'>{boleta.pelicula_titulo}</strong>
+                                <span className='bg-transparent text-custom-black-323232 text-[0.8rem]'>Show this ticket at the entrance</span>
+                            </div>
+                            <hr className='bg-custom-gray-9CA3AF w-[100%] h-[1px]'/>
+                            <div className='bg-transparent flex justify-between items-center w-[100%] h-[10%]'>
+                                <div className='bg-transparent'>
+                                    <p className='bg-transparent font-medium text-custom-black-232323'>Cinema</p>
+                                    <strong className='bg-transparent text-custom-black-232323'>CINECAMPUS</strong>
+                                </div>
+                                <img src="../../../public/campusCineLogo.jpg" alt="" className='bg-transparent object-cover h-[100%] rounded-lg'/>
+                            </div>
+                            <div className='grid grid-cols-2 grid-cols-[1fr,0.8fr] grid-rows-3 bg-transparent w-[100%] gap-4'>
+                                <div className='bg-transparent'>
+                                    <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Date</p>
+                                    <p  className='bg-transparent font-bold text-[0.8rem]'>{diasSemana[new Date(boleta.funcion_fecha).getDay()]}, {mesesAño[new Date(boleta.funcion_fecha).getMonth()]} {new Date(boleta.funcion_fecha).getDate()}th {new Date(boleta.funcion_fecha).getFullYear()}</p>
+                                </div>
+                                <div className='bg-transparent'>
+                                    <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Time</p>
+                                    <p  className='bg-transparent font-bold text-[0.8rem]'>{new Date(boleta.funcion_fecha).getHours()}:{new Date(boleta.funcion_fecha).getMinutes()}</p>
+                                </div>
+                                <div className='bg-transparent'>
+                                    <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]font-bold text-[0.8rem]'>CineCampus #</p>
+                                    <p  className='bg-transparent font-bold text-[0.8rem]'>CineCampus</p>
+                                </div>
+                                <div className='bg-transparent'>
+                                    <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Seat</p>
+                                    <p  className='bg-transparent font-bold text-[0.8rem]'>{boleta.asiento}</p>
+                                </div>
+                                <div className='bg-transparent'>
+                                    <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Total Cost</p>
+                                    <p  className='bg-transparent font-bold text-[0.8rem]'>${boleta.precio_total}</p>
+                                </div>
+                                <div className='bg-transparent'>
+                                    <p  className='bg-transparent text-custom-gray-9CA3AF font-bold text-[0.8rem]'>Order ID</p>
+                                    <p  className='bg-transparent font-bold text-[0.8rem] flex flex-wrap'>{boleta._id}</p>
+                                </div>
+                            </div>
+                            <hr className='bg-custom-black-121212 flex w-[80vw] h-[1px] m-0 p-0'/>
+                            <div className='w-[100%] bg-transparent flex justify-center items-center'>
+                                <img alt='Barcode Generator TEC-IT'
+                                src={`https://barcode.tec-it.com/barcode.ashx?data=${boleta._id}&code=Code128&translate-esc=on`}/>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </>
     )
