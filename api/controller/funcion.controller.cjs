f14a205e5ba11e9baae663c8b82150bd5717f332
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-09-02 13:43:03 -0500

    refactor: :coffin: Removing the dead code of the last presentation of the project in MongoII

--- api/controller/funcion.controller.cjs
+++ api/controller/funcion.controller.cjs
@@ -7,99 +7,6 @@ const FuncionDTO = require('../dto/funcion.dto.cjs');
 const Cliente = require('../model/usuario.model.cjs');
 const UsuarioDTO = require('../dto/usuario.dto.cjs');
 
-
-/**
- * Inserta una nueva función en la base de datos.
- * @param {Object} funcionParametro - El objeto que contiene los detalles de la funcion a insertar.
- * @property {ObjectId} pelicula_id - El ID de la película asociada a la función.
- * @property {ObjectId} sala_id - El ID de la sala donde se proyectará la película.
- * @property {Date} fecha_hora_inicio - La fecha y hora de inicio de la función.
- * @property {Date} fecha_hora_final - La fecha y hora de finalización de la función.
- * @property {number} precio_COP - El precio en COP de la función.
- *
- * @returns {Promise<Object>} Una promesa que resuelve con el resultado de la inserción o un error si se encuentra alguno.
- */
-const insertFuncion = async (funcionParametro) => {
-    let funcionInstance = new Funcion()
-    let peliculaInstance = new Pelicula()
-    let salaInstance = new Sala()
-
-    // Verifica si la película existe
-    let findPelicula = await peliculaInstance.findPeliculaById({_id: funcionParametro.pelicula_id})
-    if(!findPelicula) {
-        return {error: "La película no existe en la base de datos."}
-    }
-
-    // Verifica si la sala existe
-    let findSala = await salaInstance.findSalaById({_id: funcionParametro.sala_id})
-    if(!findSala) {
-        return {error: "La sala no existe en la base de datos."}
-    }
-
-    // Verifica si la sala está ocupada para la fecha deseada
-    let findFuncionPorSalaYFecha = await funcionInstance.findFuncion({
-            sala_id: new ObjectId('66a58fab8a7ddb4b6799c27a'),
-            $and: [
-                { fecha_hora_inicio: { $gte: funcionParametro.fecha_hora_inicio, $lte: funcionParametro.fecha_hora_final } },
-                { fecha_hora_final: { $gte: funcionParametro.fecha_hora_inicio, $lte: funcionParametro.fecha_hora_final } }
-            ]
-        }    
-    )
-    if(findFuncionPorSalaYFecha.length > 0) {
-        return { error: "La sala esta ocupada para la fecha deseada." }
-    }
-
-    // Verifica si la película está en cartelera para la fecha asignada
-    if(findPelicula.estreno > funcionParametro.fecha_hora_inicio) {
-        return { error: "La película no esta en cartelera para la fecha asignada." }
-    } else if(findPelicula.retiro < funcionParametro.fecha_hora_inicio) {
-        return { error: "La película ya se ha retirado de la cartelera para la fecha asignada." }
-    }
-
-    // Verifica si la duración de la función coincide con la duración de la película
-    let diferenciaHoraInicioYHoraFinal = funcionParametro.fecha_hora_final - funcionParametro.fecha_hora_inicio
-    let diferenciaInicioYFinalInt = diferenciaHoraInicioYHoraFinal/(1000*60)
-    if(diferenciaInicioYFinalInt != findPelicula.duracion_m) {
-        return { error: "La duración de la película no coincide." }
-    }
-
-    // Inserta la función
-    let res = await funcionInstance.insertFuncion({
-        pelicula_id: funcionParametro.pelicula_id,
-        sala_id: funcionParametro.sala_id,
-        fecha_hora_inicio: funcionParametro.fecha_hora_inicio,
-        fecha_hora_final: funcionParametro.fecha_hora_final,
-        asientos: findSala.asientos,
-        precio_COP: funcionParametro.precio_COP
-    })
-    let funcionId = res.insertedId;
-    let findFuncion = await funcionInstance.findFuncionById({_id: funcionId})
-    console.log(findFuncion)
-    return res
-}
-
-
-/**
- * Inserta una nueva función en la base de datos.
- * @param {Object} funcionParametro - El objeto que contiene el id de la funcion a buscar.
- * @returns {Promise<Object>} Una promesa que resuelve con el resultado de la busqueda o un error si se encuentra alguno.
- */
-const verificarDisponibilidadAsientos = async (funcionParametro) => {
-    let funcionInstance = new Funcion()
-    let findFuncion = await funcionInstance.findFuncionById({_id: funcionParametro})
-    if(!findFuncion) {
-        return { error: "La función no existe en la base de datos." }
-    }
-    if(findFuncion.asientos.length == 0) {
-        return { error: "La función no tiene asientos disponibles." }
-    }
-    
-    return {
-        nota: "Array de asientos disponibles",
-        asientos: findFuncion.asientos.sort()
-    }
-}
-
 //--------------------------------------------------------------------------------------------------------------------------------
 
 const verificarAsientos = async(req, res) => {
@@ -133,7 +40,6 @@ const verificarPrecioAsientos = async(req, res) => {
     let mongoUser = usuarioDTO.mongoUserToObject(process.env.VITE_MONGO_USER);
     mongoUser = await objUsuario.findOneClienteByNickOrEmail(mongoUser);
     let price = 0
-    console.log(req.body)
     if(req.body.asientos.length == 0){ 
         data = funcionDTO.templatePrecioCero();
         return res.status(data.status).json(data)
@@ -142,7 +48,6 @@ const verificarPrecioAsientos = async(req, res) => {
         data = (resModel.asientos.includes(asiento)) ? funcionDTO.templateSeating(resModel) : funcionDTO.templateNotSeating();
         if(data.status == 404) return res.status(data.status).json(data);
         resModel = (asiento.includes(resModel.preferencial)) ? funcionDTO.precioPreferencial(resModel) : funcionDTO.templateContinueWithSameObj(resModel);
-       
         resModel = (mongoUser.tipo == "VIP") ? funcionDTO.precioVip(resModel) : funcionDTO.templateContinueWithSameObj(resModel);
         data = (resModel) ? funcionDTO.templateAsientoPrice(resModel.precio) : funcionDTO.templateFuncionError(resModel);
         if(data.status == 500) return res.status(data.status).json(data);
@@ -165,11 +70,14 @@ const verificarPrecioAsiento = async(req, res) => {
     if(data.status == 404) return res.status(data.status).json(data);
     data = (resModel.asientos.length == 0) ? funcionDTO.templateNotSeating() : funcionDTO.templateSeating(resModel);
     if(data.status == 404) return res.status(data.status).json(data);
-    data = (resModel.asientos.includes(req.body.asientos)) ? funcionDTO.templateSeating(resModel) : funcionDTO.templateNotSeating();
-    if(data.status == 404) return res.status(data.status).json(data);
-    resModel = (req.body.asientos.includes(resModel.preferencial)) ? funcionDTO.precioPreferencial(resModel) : funcionDTO.templateContinueWithSameObj(resModel);
     let mongoUser = usuarioDTO.mongoUserToObject(process.env.VITE_MONGO_USER);
     mongoUser = await objUsuario.findOneClienteByNickOrEmail(mongoUser);
+
+    data = (resModel.asientos.includes(req.body.asientos)) ? funcionDTO.templateSeating(resModel) : funcionDTO.templateNotSeating();
+    if(data.status == 404) return res.status(data.status).json(data);
+    resModel = 
+
+    resModel = (req.body.asientos.includes(resModel.preferencial)) ? funcionDTO.precioPreferencial(resModel) : funcionDTO.templateContinueWithSameObj(resModel);
     resModel = (mongoUser.tipo == "VIP") ? funcionDTO.precioVip(resModel) : funcionDTO.templateContinueWithSameObj(resModel);
     data = (resModel) ? funcionDTO.templateAsientoPrice(resModel.precio) : funcionDTO.templateFuncionError(resModel);
     if(data.status == 500) return res.status(data.status).json(data);
@@ -177,8 +85,6 @@ const verificarPrecioAsiento = async(req, res) => {
 }
 
 module.exports = {
-    insertFuncion,
-    verificarDisponibilidadAsientos,
     verificarAsientos,
     verificarPrecioAsiento,
     verificarPrecioAsientos
--- api/controller/movimiento.controller.cjs
+++ api/controller/movimiento.controller.cjs
@@ -8,66 +8,6 @@ const UsuarioDTO = require('../dto/usuario.dto.cjs');
 const FuncionDTO = require('../dto/funcion.dto.cjs');
 const { verificarPrecioAsiento, verificarPrecioAsientos } = require('./funcion.controller.cjs');
 
-/**
- * Inserta un movimiento de pago en la base de datos.
- * @param {Object} movimientoParametro - Objeto que contiene los detalles del movimiento a insertar.
- * @property {ObjectId} boleta_id - El ID de la boleta asociada al movimiento.
- * @property {int} monto_COP - El monto a pagar
- *
- * @returns {Promise<Object>} Una promesa que resuelve con el resultado de la inserción.
- */
-const insertMovimiento = async (movimientoParametro) => {
-    let movimientoInstance = new Movimiento()
-    let boletaInstance = new Boleta()
-    let clienteInstance = new Cliente()
-    let funcionInstance = new Funcion()
-
-    // Busca si ya existe un movimiento asociado a la boleta
-    let findMovimiento = await movimientoInstance.findOneMovimiento({
-        boleta_id: movimientoParametro.boleta_id
-    })
-    // Busca la boleta asociada al movimiento
-    let findBoleta = await boletaInstance.findOneBoleta({
-        _id: movimientoParametro.boleta_id
-    })
-    if(!findBoleta) {
-        return { error: "La boleta no existe en la base de datos." }
-    } else if(findMovimiento) {
-        return { error: "La boleta ya esta paga." }
-    }
-
-    let findCliente = await clienteInstance.findOneCliente({
-        _id: findBoleta.cliente_id
-    })
-    let findFuncion = await funcionInstance.findFuncionById({
-        _id: findBoleta.funcion_id
-    })
-
-    // Verifica que el monto del movimiento coincida con el precio esperado para clientes VIP
-    let precioBoletaVIP = findFuncion.precio_COP*0.80
-    if(findCliente.tipo == "VIP" && movimientoParametro.monto_COP != precioBoletaVIP) {
-        return { error: "El monto de la boleta no coincide con el monto del cliente. Recuerde que por ser cliente VIP tiene un descuento del 20%. Dirijase a pagar la boleta directamente." }
-    } else if(findCliente.tipo == "Estandar" && movimientoParametro.monto_COP != findFuncion.precio_COP) {
-        return { error: "El monto de la boleta no coincide con el monto del cliente. Dirijase a pagar la boleta directamente." }
-    }
-
-    // Inserta el movimiento en la base de datos
-    let res = await movimientoInstance.insertMovimiento(movimientoParametro)
-    let movimientoId = res.insertedId
-    findMovimiento = await movimientoInstance.findOneMovimiento({
-        _id: movimientoId
-    })
-    console.log(findMovimiento)
-
-    // Actualiza el estado de pago de la boleta
-    let updateBoletaEstadoPago = await boletaInstance.updateBoleta(
-        {_id: movimientoParametro.boleta_id},
-        {$set: {estado_pago: true}}
-    )
-
-    return res
-}
-
 //--------------------------------------------------------------------------------------------------------
 
 const crearMovimiento = async(req, res) => {
@@ -81,7 +21,6 @@ const crearMovimiento = async(req, res) => {
     
     let reqUsuarioId = usuarioDTO.usuarioIdToIdKey(req.body);
     reqUsuarioId = funcionDTO.fromHexStringToObjectId(reqUsuarioId);
-    console.log(reqUsuarioId)
     let resModel = await objUsuario.findOneClienteById({...reqUsuarioId});
     let data = (resModel) ? usuarioDTO.templateExistUser(resModel) : usuarioDTO.templateNotUsers();
     if(data.status == 404) return res.status(data.status).json(data);
@@ -110,18 +49,17 @@ const crearMovimiento = async(req, res) => {
     if(typeof req.body.asientos == "object"){
         await verificarPrecioAsientos(simulatedReq, simulatedRes);
     } else if(typeof req.body.asientos == "string"){
-        await verificarPrecioAsiento(simulatedReq, simulatedRes)
+        await verificarPrecioAsientos(simulatedReq, simulatedRes)
     }
     let precioPagar = simulatedRes
     data = (req.body.monto_COP != precioPagar.data.precio) ? funcionDTO.templateIncorrectPaymente() : funcionDTO.templateContinue();
     if(data.status == 400) return res.status(data.status).json(data);
     reqUsuarioId = usuarioDTO.idKeyToUsuarioId(reqUsuarioId);
     reqUsuarioId = movimientoDTO.fromHexStringToObjectId(reqUsuarioId)
-    console.log(reqUsuarioId)
     resModel = await objMovimiento.insertMovimiento({cliente_id: reqUsuarioId.cliente_id, monto_COP: req.body.monto_COP});
     data = (resModel.acknowledged) ? movimientoDTO.templateMovimientoSaved({_id: resModel.insertedId, cliente_id: reqUsuarioId.cliente_id, monto_COP: req.body.monto_COP}) : movimientoDTO.templateMovimientoError(resModel);
     if(data.status == 500) return res.status(data.status).json(data);
     return res.status(data.status).json(data);
 }
 
-module.exports = {insertMovimiento, crearMovimiento}
+module.exports = {crearMovimiento}
